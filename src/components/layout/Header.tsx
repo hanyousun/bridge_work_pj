f92f059f7a376a6dc11d856f5d2129044cba7f87
sun <h092612@daum.net>
2025-08-31 07:49:34 +0900

    [Chore] 버튼 스타일 변경

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -17,7 +17,7 @@ export default function sidebar() {
         <p className="text-xs">
           <span className="pr-1 text-primary font-bold">홍길동님</span>환영합니다.
         </p>
-        <Button variant="transparent" size="icon">
+        <Button variant="ghost" size="icon">
           <LogIn className="size-4" strokeWidth={1.5} />
           {/* <LogOut className="size-4" strokeWidth={1.5} /> */}
         </Button>
